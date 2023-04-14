322be74ba0a891efceef03624da077e5c4aeeca5
Evgeniy Mukhin <muhin@intervolga.ru>
2023-04-14 16:32:54 +0300

    Добавлены footer и header

---
+++ app/views/templates/header.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>#TITLE#</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+	<div class="container-fluid">
+		<a class="navbar-brand" href="/stat/">EcoCar</a>
+		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+				<li class="nav-item">
+					<a class="nav-link active" aria-current="page" href="/stat/">Моё авто</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="/profile/">Личный кабинет</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="/question/">Задать вопрос</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link link-danger" href="/logout/">Выйти</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</nav>
+<div class="container-fluid d-flex h-100 justify-content-center align-items-center p-0">
+	<div class="col-sm-12 col-md-8 col-md-offset-2 col-lg-6">
